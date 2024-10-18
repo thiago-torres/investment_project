f5ed4da8bb7a2161c7e90ca2c5a00554be33c83f
Thiago Torres <thiago.torres@outlook.com.br>
2024-10-18 17:06:26 -0300

    add route analyze assets

--- app.py
+++ app.py
@@ -8,14 +8,6 @@ view_manager = ViewManager()
 def index():
     return render_template('index.html')
 
-@app.route('/buttons')
-def buttons():
-    return render_template('buttons.html')
-
-@app.route('/cards')
-def cards():
-    return render_template('cards.html')
-
 @app.route('/blank')
 def blank():
     return render_template('blank.html')
@@ -24,6 +16,32 @@ def blank():
 def page404():
     return render_template('404.html')
     
+@app.route('/api/analyze-global-assets', methods=['POST'])
+def analyze_global_assets():
+    selected = request.form.get('selected')
+    result = view_manager.analyze_global_assets(selected)
     
+    if result:
+        return result
+    else:
+        return jsonify({'error': 'No assets analyzed.'}), 404
+
+@app.route('/api/analyze-personal-assets', methods=['POST'])
+def analyze_personal_assets():
+
+    selected = request.form.get('selected')
+    asset_type = request.form.get('assetType')
+    other_asset = request.form.get('otherAsset')
+
+    if asset_type == 'outros':
+        asset_type = other_asset
+
+    result = view_manager.analyze_personal_assets(selected, asset_type)
+    
+    if result:
+        return result
+    else:
+        return jsonify({'error': 'No assets analyzed.'}), 404
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
