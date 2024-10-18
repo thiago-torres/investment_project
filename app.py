dadfcfc5dd69e708458290697431dd2f37855559
Thiago Torres <thiago.torres@outlook.com.br>
2024-10-18 13:48:14 -0300

    initial feature new template commit

--- app.py
+++ app.py
@@ -8,40 +8,22 @@ view_manager = ViewManager()
 def index():
     return render_template('index.html')
 
-@app.route('/global-assets')
-def global_assets():
-    return render_template('global-assets.html')
+@app.route('/buttons')
+def buttons():
+    return render_template('buttons.html')
 
-@app.route('/personal-assets')
-def personal_assets():
-    return render_template('personal-assets.html')
+@app.route('/cards')
+def cards():
+    return render_template('cards.html')
 
-@app.route('/api/analyze-global-assets', methods=['POST'])
-def analyze_global_assets():
-    selected = request.form.get('selected')
-    result = view_manager.analyze_global_assets(selected)
+@app.route('/blank')
+def blank():
+    return render_template('blank.html')
+
+@app.route('/404')
+def page404():
+    return render_template('404.html')
     
-    if result:
-        return result
-    else:
-        return jsonify({'error': 'No assets analyzed.'}), 404
-
-@app.route('/api/analyze-personal-assets', methods=['POST'])
-def analyze_personal_assets():
-
-    selected = request.form.get('selected')
-    asset_type = request.form.get('assetType')
-    other_asset = request.form.get('otherAsset')
-
-    if asset_type == 'outros':
-        asset_type = other_asset
-
-    result = view_manager.analyze_personal_assets(selected, asset_type)
     
-    if result:
-        return result
-    else:
-        return jsonify({'error': 'No assets analyzed.'}), 404
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
